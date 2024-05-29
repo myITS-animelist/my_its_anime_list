e6fc4cc0255ed169955e0846b65af034dc786e37
AdonisZK <adonisvefleyor@gmail.com>
2024-05-29 20:20:01 +0700

    feat: firebaseAuth

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,3 +1,50 @@
-class Failure {
+import 'package:equatable/equatable.dart';
+
+class Failure extends Equatable{
   String? get message => null;
+  
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class OfflineFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+class ServerFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+class WeekPassFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+class ExistedAccountFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class NoUserFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+class WrongPasswordFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+class UnmatchedPassFailure extends Failure{
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+class NotLoggedInFailure extends Failure{
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+class EmailVerifiedFailure extends Failure{
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+class TooManyRequestsFailure extends Failure{
+  @override
+  List<Object?> get props => throw UnimplementedError();
 }
